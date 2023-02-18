673f23d1b783de194524b1942b7315c6e8396f91
Lyndon <lyndon@hesh.co.uk>
2023-02-18 02:38:07 +0000

    Implement Pack Create(), PrintPack(), Deal(), DealCard(int)

--- CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -9,11 +9,41 @@ namespace CMP1903M_A01_2223
 {
     class Pack
     {
-        public List<Card> pack;
+        public List<Card> pack = new List<Card>();
 
-        public Pack()
+        public void Create()
         {
-
+            Card[] deck = new Card[52];
+            int j = 1;
+            for (int i = 0; i < 13; i++)
+            {
+                deck[i] = new Card(j, 0);
+                j++;
+            }
+            j = 1;
+            for (int i = 13; i < 26; i++)
+            {
+                deck[i] = new Card(j, 1);
+                j++;
+            }
+            j = 1;
+            for (int i = 26; i < 39; i++)
+            {
+                deck[i] = new Card(j, 2);
+                j++;
+            }
+            j = 1;
+            for (int i = 39; i < 52; i++)
+            {
+                deck[i] = new Card(j, 3);
+                j++;
+            }
+            Console.WriteLine("There are " + deck.Length + " cards on the table. Putting them into a pack...");
+            foreach (Card card in deck)
+            {
+                pack.Add(card);
+            }
+            Console.WriteLine("There are " + pack.Count + " cards in the pack.");
         }
 
         public static bool ShuffleCardPack(int typeOfShuffle)
@@ -22,15 +52,35 @@ namespace CMP1903M_A01_2223
 
             return false;
         }
-        public static Card Deal()
+        public Card Deal()
         {
-            //Deals one card
+            Console.WriteLine("Card Dealt: " + this.pack.ElementAt(0));
+            this.pack.Remove(this.pack.ElementAt(0));
+            Console.WriteLine("Only " + this.pack.Count + " cards remain.");
             return null;
         }
-        public static List<Card> DealCard(int amount)
+        public List<Card> DealCard(int amount)
         {
-            //Deals the number of cards specified by 'amount'
-            return null;
+            List<Card> cards = new List<Card>();
+            for (int i = 0; i < amount; i++)
+            {
+                cards.Add(pack.ElementAt(i));
+                this.pack.Remove(this.pack.ElementAt(i));
+            }
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.ToString());
+            }
+            Console.WriteLine("Only " + this.pack.Count + " cards remain.");
+            return cards;
+        }
+        public void PrintPack()
+        {
+            foreach(Card card in this.pack)
+            {
+                Console.WriteLine(card.ToString());
+            }
+            Console.WriteLine("There are " + this.pack.Count + " cards in the pack.");
         }
     }
 }
--- CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
@@ -10,40 +10,20 @@ namespace CMP1903M_A01_2223
     {
         static void Main(string[] args)
         {
-            Card[] deck = new Card[52];
-            int j = 1;
-            for (int i = 0; i < 13; i++)
-            {
-                deck[i] = new Card(j, 0);
-                j++;
-            }
-            j = 1;
-            for (int i = 13; i < 26; i++)
-            {
-                deck[i] = new Card(j, 1);
-                j++;
-            }
-            j = 1;
-            for (int i = 26; i < 39; i++)
-            {
-                deck[i] = new Card(j, 2);
-                j++;
-            }
-            j = 1;
-            for (int i = 39; i < 52; i++)
-            {
-                deck[i] = new Card(j, 3);
-                j++;
-            }
-            j = 0;
-            foreach (Card card in deck)
-            {
-                Console.WriteLine(card.ToString());
-            }
-            Console.ReadLine();
-
             Pack pack = new Pack();
-            
+            pack.Create();
+            pack.PrintPack();
+            Console.ReadLine();
+            pack.DealCard(13);
+            Console.ReadLine();
+            pack.Deal();
+            Console.ReadLine();
+            pack.Deal();
+            Console.ReadLine();
+            pack.DealCard(10);
+            Console.ReadLine();
+            pack.PrintPack();
+            Console.ReadLine();
         }
     }
 }
