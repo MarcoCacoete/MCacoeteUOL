49a1f1ed27111646bcb477c9260cb35d0c88a1b0
Marco Caçoete <90009471+MasterOsadam@users.noreply.github.com>
2023-02-21 14:56:36 +0000

    Re added my pseudo code and notes file, which is incomplete until I find the original one that was lost. Struggling to work out how to show the pack object to shuffling method.

--- CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 namespace CMP1903M_A01_2223
 {
     class Pack
-    {     
+    {
         //constructor
-        public  Pack()
-        {           
+        public Pack()
+        {
 
             Card[] pack = new Card[52]; //empty array with 52 spaces for 52 card objects 
 
@@ -19,103 +19,91 @@ namespace CMP1903M_A01_2223
                                //without going out of index
             int suit = 0; //suit variable for second iteration
 
-                // Card pack generator
+            // Card pack generator
 
-                while (packSize < 51) // while loop lets it run until it fills a pack with 52 cards
+            while (packSize < 51) // while loop lets it run until it fills a pack with 52 cards
+            {
+
+                while (suit < 4) //iterates for each of the 4 suits and creates suited cards until all 13 are in
                 {
-
-                    while (suit < 4) //iterates for each of the 4 suits and creates suited cards until all 13 are in
+                    int suitCards = 0;
+                    suit++;
+                    while (suitCards < 13)
                     {
-                        int suitCards = 0;
-                        suit++;
-                        while (suitCards < 13)
-                        {
 
-                            suitCards++;
-                            // deck = deck.Append((suitCards)).ToArray();
-                            //Card card = new Card(value, suit);
+                        suitCards++;
+                        // deck = deck.Append((suitCards)).ToArray();
+                        //Card card = new Card(value, suit);
 
-                            packSize++;                                   //counter iterator for packsize to stop operation when pack is complete
-                            pack[packSize] = new Card(suitCards, suit);
-                            string testout = pack[packSize].ToString();
-                            Console.WriteLine(testout);
-                       
-                        }
+                        packSize++;                                   //counter iterator for packsize to stop operation when pack is complete
+                        pack[packSize] = new Card(suitCards, suit);
+                        string testout = pack[packSize].ToString();
+                        Console.WriteLine(testout);
 
                     }
-                
 
                 }
 
-                // Card c = new Card(value, suit);
+
+            }
+
+            // Card c = new Card(value, suit);
 
 
-                /*foreach (Card card in deck)
+            /*foreach (Card card in deck)
 
-                     try
-                     {
-                         Console.WriteLine(card.ToString());
-                         // c.Card(value, suit);
-                         //Console.WriteLine("[{0}]", string.Join(", ", deck));
-                         //Console.WriteLine(suit);
-                         Console.WriteLine(packSize);
-                         Console.ReadLine();
+                 try
+                 {
+                     Console.WriteLine(card.ToString());
+                     // c.Card(value, suit);
+                     //Console.WriteLine("[{0}]", string.Join(", ", deck));
+                     //Console.WriteLine(suit);
+                     Console.WriteLine(packSize);
+                     Console.ReadLine();
 
-                     }
-                     catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                   }*/
-                Console.ReadLine();
+                 }
+                 catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+               }*/
+            Console.ReadLine();
 
-                    
-                }
 
-            
+        }
 
-        public static bool shuffleCardPack(int typeOfShuffle)
+        public static bool shuffleCardPack(int typeOfShuffle, Pack pack)
         {
             //Shuffles the pack based on the type of shuffle
 
-            /*  if (shuffleType == 2)
-              {
+            if (typeOfShuffle == 1)
+            {
+                var array = new int[] { 1, 2, 3, 4, 5, 6 };
+                var rnd = new Random();
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    int j = i + rnd.Next(array.Length - i);
+                    var tmp = array[j];
+                    array[j] = array[i];
+                    array[i] = tmp;
+                }
+                Console.WriteLine(array);
 
+            }
 
-                  int[] shuffle()
-                  {
-
-                      for (int i = pack.Length; i > 1; i--)
-                      {
-                          int j = Random.Next(i);
-                          int temp = cards[j];
-                          cards[j] = cards[i - 1];
-                          cards[i - 1] = temp;
-                      }
-                      return cards;
-                  }
-
-                  int[] deal(int N)
-                  {
-                      int[] result = new int[N];
-                      Array.Copy(shuffle(), 0, result, N);
-                      return result;
-                  }
-              }
-                  }
-                      public static Card deal()
-                      {
-                          //Deals one card
-
-                      }
-                      public static List<Card> dealCard(int amount)
-                      {
-                          //Deals the number of cards specified by 'amount'
-                    */
             Console.WriteLine("I can shuffle now Dave");
+            Console.ReadLine();
             return true;
         }
+        /* public static Card deal()
+         {
+             //Deals one card
+
+         }
+         public static List<Card> dealCard(int amount)
+         {
+             //Deals the number of cards specified by 'amount'
+         }*/
     }
+
 }
-
-
       
